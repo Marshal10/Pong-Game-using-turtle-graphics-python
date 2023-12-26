347a450a9dd6cfe38749794b6523fa1f503d02d5
Marshal10 <marshalmaleane123gmail.com>
2023-12-26 20:43:34 +0530

    Refactored into one turtle element

--- main.py
+++ main.py
@@ -2,33 +2,33 @@ from turtle import Screen,Turtle
 from paddle import Paddle
 import time
 
-STARTING_COMPUTER_POSITION=[(410,0),(410,-20),(410,-40)]
 
 screen=Screen()
 screen.bgcolor("black")
+screen.title("Pong")
 screen.tracer(0)
 screen.listen()
-screen.setup(900,700)
+screen.setup(800,600)
 
 paddle1=Paddle()
 paddle2=Paddle()
 
-for i in range(3):
-    paddle2.segments[i].goto(STARTING_COMPUTER_POSITION[i])
+paddle2.head.goto((370,0))
 #screen.onkey(paddle1.move_up,"w")
 #screen.onkey(paddle1.move_down,"s")
+
 screen.onkey(paddle1.move_up,"Up")
 screen.onkey(paddle1.move_down,"Down")
-
 while True:
     screen.update()
     time.sleep(0.1)
-    if paddle2.head.ycor()==340:
-        paddle2.move_down()
-    elif paddle2.head.ycor()==-340:    
-        paddle2.move_up()
-    else:
-        paddle2.move()
+    
+      
+
+    
+    
+    paddle2.move()
+    
     
 
     
--- paddle.py
+++ paddle.py
@@ -1,46 +1,39 @@
 from turtle import Turtle
-STARTING_POSITION=[(-420,0),(-420,-20),(-420,-40)]
 
 MOVING_DISTANCE=20
 class Paddle():
 
     def __init__(self):
-        super().__init__()
-        self.segments=[]
+        self.paddle=[]
         self.create_paddle()
-        self.head=self.segments[0]
+        self.head=self.paddle[0]
     
     def create_paddle(self):
-        for position in STARTING_POSITION:
-            self.add_segment(position)
-            
-        
-    def add_segment(self,position):
         segment=Turtle(shape='square')
         segment.color('white')
         segment.penup ()
-        segment.goto(position)
-        segment.left(90)
-        self.segments.append(segment)
+        segment.shapesize(stretch_len=5,stretch_wid=1)
+        segment.goto((-380,0))
+        segment.left(90) 
+        self.paddle.append(segment)
+        
+        
         
     def move(self):
-        for seg_num in range(len(self.segments)-1,0,-1):
-            new_x=self.segments[seg_num-1].xcor()
-            new_y=self.segments[seg_num-1].ycor()
-            self.segments[seg_num].goto(new_x,new_y)
+        if self.head.ycor()==260:
+            self.head.setheading(270)
+        elif self.head.ycor()==-240:
+            self.head.setheading(90)
         self.head.forward(MOVING_DISTANCE)
+
         
     def move_down(self):
-        if self.head.heading()==90:
-            self.segments=self.segments[::-1]
-            self.head=self.segments[0]
-            self.head.setheading(270)
-        self.move()
+        if self.head.ycor()==-240:
+            return None
+        self.head.backward(MOVING_DISTANCE)
         
     def move_up(self):
-        if self.head.heading()==270:
-            self.segments=self.segments[::-1]
-            self.head=self.segments[0]
-            self.head.setheading(90)
-        self.move()
+        if self.head.ycor()>250:
+            return None
+        self.head.forward(MOVING_DISTANCE)
         
